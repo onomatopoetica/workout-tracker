4d130308d9241f3a5d918908f12194dc9011182f
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-03-19 11:23:37 -0700

    Testing db connection

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/maniacal-cake',
+    process.env.MONGODB_URI || 'mongodb://localhost/maniacal-pancake',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
