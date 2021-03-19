bee46ea7960d07b56f9a5112cb24f3cf1ae13c13
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2021-03-19 10:53:14 -0700

    Testing database + pushing to Heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/maniacal-cake01',
+    process.env.MONGODB_URI || 'mongodb://localhost/maniacal-cakes',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
